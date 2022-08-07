e7e3973fdca437df5d5fb2840ac80e6201500d59
sorinies <7258136+sorinies@users.noreply.github.com>
2022-08-07 13:32:38 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import "../styles/common.scss"
 import * as React from "react"
-import Header from "../components/header"
+import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Main from "../components/Main"
 
