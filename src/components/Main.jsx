df6bd9e63e7431d45390207f88700c25dc0a6319
sorinies <7258136+sorinies@users.noreply.github.com>
2022-09-09 18:56:45 +0900

    메뉴 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,10 +6,12 @@ function Main() {
     <>
     <nav className="gnb">
       <ul>
-        <li><a href="https://twitter.com/Padaseoul" target="_blank" rel="noreferrer">Twitter</a></li>
+        <li><a href="https://twitch.tv/padaseoul" target="_blank" rel="noreferrer">Twitch</a></li>
+        <li><a href="https://twitter.com/PadaSeoul" target="_blank" rel="noreferrer">Twitter</a></li>
         <li><a href="https://instagram.com/padaseoul" target="_blank" rel="noreferrer">Instagram</a></li>
         <li><Link to="/discord">Discord</Link></li>
-        <li><a href="https://forms.gle/cXy2MpRTt2N29mvY9" target="_blank" rel="noreferrer">대관문의</a></li>
+        <li><a href="https://twitch.tv/padaseoul" target="_blank" rel="noreferrer">Twitch</a></li>
+        {/* <li><a href="https://forms.gle/cXy2MpRTt2N29mvY9" target="_blank" rel="noreferrer">대관문의</a></li> */}
       </ul>
     </nav>
     </>
